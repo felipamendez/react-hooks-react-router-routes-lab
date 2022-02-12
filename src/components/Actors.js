0d6c6ffde7fba438a8cf63a5144d9109996a0ec4
Felipa Mendez <felipalouise@gmail.com>
2022-02-11 18:42:11 -0600

    commit tree 325be6ca08d90f00e3579cd1ef6bb5d47e609d31

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,26 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map((actor) => {
+        return (
+          <>
+          <div>{actor.name}</div>
+          <ul>
+            {actor.movies.map((movie) => {
+              return (
+                <li>{movie}</li>
+              )
+            })}
+          </ul>
+          </>
+        )
+      })}
+    </div>
+  
+  )
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,28 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div>
+    <h1>Movies Page</h1>
+    {movies.map((movie) => { 
+     return  (
+      <>
+        <p> Title: {movie.title} </p>
+        <p>  Time: {movie.time} </p>
+        <p>Genres:</p>
+        <ul>
+         {movie.genres.map((genre) => { 
+           return (
+            <li>
+              {genre}
+            </li>
+            )})}
+        </ul>
+      </>
+      )
+    })}
+  </div>
+  )
 }
 
 export default Movies;
